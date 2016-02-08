6bd5880ecf04bd2d53e6261d9c3b6aa1abf1e2a2
lunaticedit <tim.sarbin@gmail.com>
2016-02-07 23:30:32 -0500

    Fixed weird formatting issue from last checkin.

--- src/player.cpp
+++ src/player.cpp
@@ -82,86 +82,86 @@ void Player::WalkPlayer(eDirection direction) {
 }
 
 std::shared_ptr<ITool> Player::GetCurrentTool() {
-return std::shared_ptr<ITool>(this->CurrentTool);
+   return std::shared_ptr<ITool>(this->CurrentTool);
 }
 
 std::vector<std::shared_ptr<IProp>> Player::GetInventory() {
-return this->Inventory;
+   return this->Inventory;
 }
 
 void Player::SpawnIntoInventory(std::shared_ptr<IProp> prop) {
-this->Inventory.push_back(std::shared_ptr<IProp>(prop));
+   this->Inventory.push_back(std::shared_ptr<IProp>(prop));
 }
 
 void Player::PickUpItemFromGround() {
-auto currentLandmark = GameState::Get().GetCurrentLandmark();
-auto prop = currentLandmark->GetProp(this->GetPositionX(), this->GetPositionY());
-if (prop == nullptr) {
-GameState::Get().AddLogMessage("There is nothing on the ground to pick up.");
-return;
-}
+   auto currentLandmark = GameState::Get().GetCurrentLandmark();
+   auto prop = currentLandmark->GetProp(this->GetPositionX(), this->GetPositionY());
+   if (prop == nullptr) {
+      GameState::Get().AddLogMessage("There is nothing on the ground to pick up.");
+      return;
+   }
 
-currentLandmark->RemoveProp(this->GetPositionX(), this->GetPositionY());
-this->SpawnIntoInventory(prop);
-bool startsWithVowel = prop->GetName().find_first_of("aAeEiIoOuU") == 0;
-GameState::Get().AddLogMessageFmt("You pick up %s %s.", startsWithVowel ? "an" : "a", prop->GetName().c_str());
+   currentLandmark->RemoveProp(this->GetPositionX(), this->GetPositionY());
+   this->SpawnIntoInventory(prop);
+   bool startsWithVowel = prop->GetName().find_first_of("aAeEiIoOuU") == 0;
+   GameState::Get().AddLogMessageFmt("You pick up %s %s.", startsWithVowel ? "an" : "a", prop->GetName().c_str());
 }
 
 void Player::EquipFromInventory(std::shared_ptr<ITool> tool) {
-this->RemoveFromInventory(std::dynamic_pointer_cast<IProp>(tool));
-this->CurrentTool = std::shared_ptr<ITool>(tool);
+   this->RemoveFromInventory(std::dynamic_pointer_cast<IProp>(tool));
+   this->CurrentTool = std::shared_ptr<ITool>(tool);
 }
 
 void Player::DropInventoryItemOnGround(std::shared_ptr<IProp> prop) {
-auto currentLandmark = GameState::Get().GetCurrentLandmark();
-this->RemoveFromInventory(prop);
-currentLandmark->AddProp(this->GetPositionX(), this->GetPositionY(), prop);
+   auto currentLandmark = GameState::Get().GetCurrentLandmark();
+   this->RemoveFromInventory(prop);
+   currentLandmark->AddProp(this->GetPositionX(), this->GetPositionY(), prop);
 }
 
 void Player::RemoveFromInventory(std::shared_ptr<IProp> prop) {
-auto i = 0;
-for (auto invProp : this->Inventory) {
-if (invProp != prop) {
-i++;
-continue;
-}
+   auto i = 0;
+   for (auto invProp : this->Inventory) {
+      if (invProp != prop) {
+         i++;
+         continue;
+      }
 
-this->Inventory.erase(this->Inventory.begin() + i);
-break;
-}
+      this->Inventory.erase(this->Inventory.begin() + i);
+      break;
+   }
 }
 
 void Player::UseTool() {
-if(this->CurrentTool == nullptr) {
-GameState::Get().AddLogMessage("You do not currently have a tool equipped!");
-return;
-}
-if (!this->CurrentTool->IsUsable()) {
-auto prop = std::dynamic_pointer_cast<IProp>(this->CurrentTool);
+   if (this->CurrentTool == nullptr) {
+      GameState::Get().AddLogMessage("You do not currently have a tool equipped!");
+      return;
+   }
+   if (!this->CurrentTool->IsUsable()) {
+      auto prop = std::dynamic_pointer_cast<IProp>(this->CurrentTool);
 
-GameState::Get().AddLogMessageFmt("The %s is not usable at this time.", prop->GetName().c_str());
-}
-this->CurrentTool->Use();
+      GameState::Get().AddLogMessageFmt("The %s is not usable at this time.", prop->GetName().c_str());
+   }
+   this->CurrentTool->Use();
 }
 
 void Player::UnequipCurrentTool() {
-if (this->CurrentTool == nullptr) {
-GameState::Get().AddLogMessage("You do not currently have a tool equipped!");
-return;
-}
-this->SpawnIntoInventory(std::dynamic_pointer_cast<IProp>(this->CurrentTool));
-this->CurrentTool = nullptr;
+   if (this->CurrentTool == nullptr) {
+      GameState::Get().AddLogMessage("You do not currently have a tool equipped!");
+      return;
+   }
+   this->SpawnIntoInventory(std::dynamic_pointer_cast<IProp>(this->CurrentTool));
+   this->CurrentTool = nullptr;
 }
 
 bool Player::IsPassable(int x, int y) {
-auto currentLandmark = GameState::Get().GetCurrentLandmark();
-auto tile = currentLandmark->GetTile(x, y);
-if (!TileHasSurfaceAttribute(tile, eSurfaceAttributeType::Walkable)) {
-return false;
-}
-auto prop = currentLandmark->GetProp(x, y);
-if (prop == nullptr) {
-return true;
-}
-return prop->GetIsPassable();
+   auto currentLandmark = GameState::Get().GetCurrentLandmark();
+   auto tile = currentLandmark->GetTile(x, y);
+   if (!TileHasSurfaceAttribute(tile, eSurfaceAttributeType::Walkable)) {
+      return false;
+   }
+   auto prop = currentLandmark->GetProp(x, y);
+   if (prop == nullptr) {
+      return true;
+   }
+   return prop->GetIsPassable();
 }
