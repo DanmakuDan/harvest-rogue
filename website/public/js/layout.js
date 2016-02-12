738a71ba10b3a110ae8d01baf28554195c398a6b
Tim Sarbin <lunaticedit@live.com>
2016-02-12 17:31:02 -0500

    More website updates.

---
+++ website/public/js/layout.js
@@ -0,0 +1,21 @@
+$(function() {
+   $('.main.menu').visibility({
+      type: 'fixed'
+   });
+   $('.overlay').visibility({
+      type: 'fixed',
+      offset: 80
+   });
+
+   // lazy load images
+   $('.image').visibility({
+      type: 'image',
+      transition: 'vertical flip in',
+      duration: 500
+   });
+
+   // show dropdown on hover
+   $('.main.menu  .ui.dropdown').dropdown({
+      on: 'hover'
+   });    
+})
--- website/server.js
+++ website/server.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(express.static(__dirname + '/public'))
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+
+
+app.get('/', function(req, res) {
+    res.render('pages/index', { pageTitle: 'Home' }); 
 });
+
+app.get('/screenshots', function(req, res) {
+    res.render('pages/screenshots', { pageTitle: 'Screenshots' }); 
+});
+
+app.get('/forums', function(req, res) {
+    res.render('pages/forums', { pageTitle: 'Forums' }); 
+});
+
+app.get('/login', function(req, res) {
+    res.render('pages/login', { pageTitle: 'Account Login' }); 
+});
+
+
+
 
 app.listen(80, function () {
   console.log('Listening on port 80..');
