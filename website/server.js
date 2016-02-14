7ba203c8d60df25669bfab9cf88cfb0049f6d421
Tim Sarbin <lunaticedit@live.com>
2016-02-14 17:20:18 -0500

    website updates

--- website/server.js
+++ website/server.js
@@ -68,7 +68,7 @@ function getForums(callbackPass, callbackFail) {
 function getForumPosts(forumName, callbackPass, callbackFail) {
    updateForumViewCount(forumName, function() {
       sqlConnect(function(c) {
-         c.query('SELECT * FROM ForumPost WHERE ForumId IN (SELECT Id FROM Forum WHERE Name = ? LIMIT 1) ORDER BY CreatedOn DESC', 
+         c.query('SELECT * FROM ForumPost WHERE ForumId IN (SELECT Id FROM Forum WHERE Name = ?) ORDER BY CreatedOn DESC', 
          [forumName], function(err, results) {
             c.destroy();
             if (results == null || results.length == 0) {
